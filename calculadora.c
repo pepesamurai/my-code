6a65c2ce0aed4092b3c68f4fae4fb86e36bc20a5
pepesamurai <samuraipepe45@gmail.com>
2018-06-26 21:27:11 -0300

    Criando o arquivo calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(main){
+int n1,n2;//Pega os 
+printf("Digite um numero: ");
+scanf("%i",&n1);
+printf("Digite outro numero: ");
+scanf("%i",&n2);
+printf("Divisão: " n1 / n2);
+printf("Multiplicação: " n1 * n2);
+printf("Subtração: " n1 - n2);
+printf("Adição: " n1 + n2);
+
+}
