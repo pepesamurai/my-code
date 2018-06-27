7c7783feb126439e28e3937dff6cb4d991f4b258
pepesamurai <samuraipepe45@gmail.com>
2018-06-26 21:43:39 -0300

    Criação do arquivo idade.c e codificação

---
+++ idade.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+unsigned int idade,ano;
+printf("Digite o ano que você nasceu: ");
+scanf("%i",&ano);
+idade = 2018 - ano;
+printf("Idade = %i",idade);
+return 0;
+}
