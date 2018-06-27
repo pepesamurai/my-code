fa5939007b95cfdf6a48d63e930242dc66b5ee6c
pepesamurai <samuraipepe45@gmail.com>
2018-06-27 11:27:17 -0300

    Criando e programando o programa criando_tipo.c

---
+++ criando_tipo.c
@@ -0,0 +1,13 @@
+//Name:criando_tipo.c
+//Author:pepesamurai
+#include <stdio.h>
+typedef unsigned int PO;
+typedef int NE;
+int main(){
+PO numero_positivo = 50;
+NE numero_negativo = -50;
+printf("Variavel positiva = PO\n");
+printf("Variavel negativa = NE\n");
+printf("Numero positivo: %i\n",numero_positivo);
+printf("Numero negativo: %i\n",numero_negativo);
+}
