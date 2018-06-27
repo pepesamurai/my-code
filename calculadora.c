31251f06b1e4b07c0300606feb513a4d9a227a26
pepesamurai <samuraipepe45@gmail.com>
2018-06-26 21:34:12 -0300

    Alteração no modo de fazer as contas

--- calculadora.c
+++ calculadora.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-void main(main){
-int n1,n2;//Pega os 
+int main(){
+int n1,n2,total;//Pega os 
 printf("Digite um numero: ");
 scanf("%i",&n1);
 printf("Digite outro numero: ");
 scanf("%i",&n2);
-printf("Divisão: " n1 / n2);
-printf("Multiplicação: " n1 * n2);
-printf("Subtração: " n1 - n2);
-printf("Adição: " n1 + n2);
-
+total = n1 / n2;
+printf("Divisão: %i\n",total);
+total = n1 * n2;
+printf("Multiplicação: %i\n",total);
+total = n1 - n2;
+printf("Subtração: %i\n",total);
+total = n1 + n2;
+printf("Adição: %i\n",total);
+return 0;
 }
